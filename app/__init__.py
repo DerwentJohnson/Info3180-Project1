5b5506a75cb2fc7aca939d4eeed58a9515728852
DerentJohnson <derwent.johnson@gmail.com>
2020-04-15 19:58:08 -0500

    message

--- app/__init__.py
+++ app/__init__.py
@@ -3,7 +3,7 @@ from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 SECRET_KEY = "change this to be a more random key"
-SQLALCHEMY_DATABASE_URI = "postgresql://postgres:password123@localhost/Profile"
+SQLALCHEMY_DATABASE_URI = 'postgresql://umtkumbfvedjlk:5dae1bc6c44a647841f8838351db9317ee2b8f5dcd38a8d0faf90d4ed7bba3a7@ec2-23-20-129-146.compute-1.amazonaws.com:5432/dckbe76c50r1ds'
 SQLALCHEMY_TRACK_MODIFICATIONS = True # added just to suppress a warning
 UPLOAD_FOLDER = './app/static/uploads'
 
